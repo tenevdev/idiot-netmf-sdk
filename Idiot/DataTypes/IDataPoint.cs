71d15ff713a7324f75f123137f02dc9c5f252b1b
Tencho Tenev <tenev.dev@outlook.com>
2015-02-23 18:54:37 +0200

    Add project skeleton

---
+++ Idiot/DataTypes/IDataPoint.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Idiot.DataTypes
+{
+    class IDataPoint
+    {
+    }
+}
---
+++ Idiot/Net/Credentials.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Idiot.Net
+{
+    class Credentials
+    {
+    }
+}
