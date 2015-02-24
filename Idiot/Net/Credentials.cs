6861b7f9d994ddc3c1a7e9ef046f64447fff32ce
Tencho Tenev <tenev.dev@outlook.com>
2015-02-24 17:16:45 +0200

    Add credentials class for user authorization

--- Idiot/Net/Credentials.cs
+++ Idiot/Net/Credentials.cs
@@ -1,9 +1,33 @@
 using System;
 using Microsoft.SPOT;
+using System.Text;
+using GHIElectronics.NETMF.Net;
 
 namespace Idiot.Net
 {
-    class Credentials
+    public class Credentials
     {
+        private string username;
+
+        private string password;
+
+        public Credentials(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+
+            this.AuthorizationHeader = this.toAuthorizationHeader();
+        }
+
+        /// <summary>
+        /// Basic authorization header value to be used for server authentication
+        /// </summary>
+        public string AuthorizationHeader { get; private set; }
+
+        private string toAuthorizationHeader()
+        {
+            return "Basic " + ConvertBase64.ToBase64String(Encoding.UTF8.GetBytes(this.username + ":" + this.password));
+        }
+
     }
 }
